cb85655de7576f34e1c1c9f2b5bd16b3afe97225
Michał Warchoł <mwarchol33@gmail.com>
2023-08-27 01:08:20 +0200

    feat: Add edit and delete functionality + remove message files

--- src/EventManager/EventManager.messages.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  enterName: 'Enter event name: ',
-  enterDescription: 'Enter event description: ',
-  enterDate: 'Date (yyyy-mm-dd hh:mm:ss): ',
-  noData: 'No events found!',
-};
--- src/Menu/Menu.messages.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  choose: 'Please choose an option:',
-  showCloseEvents: 'Show upcoming events',
-  createEvent: 'Create a new event',
-  manageEvents: 'Manage events',
-  exit: 'Exit',
-};
